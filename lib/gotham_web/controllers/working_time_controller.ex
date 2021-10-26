0310e2c2991c04ec36043807f2364b1973c0d316
Mathieu <arthur.mathieu@epitech.eu>
2021-10-26 16:29:47 +0200

    post method working_time

--- lib/gotham_web/controllers/working_time_controller.ex
+++ lib/gotham_web/controllers/working_time_controller.ex
@@ -1,6 +1,6 @@
 defmodule GothamWeb.WorkingTimeController do
   use GothamWeb, :controller
-  require Logger
+
   alias Gotham.WorkingTimeController
   alias Gotham.WorkingTimeController.WorkingTime
 
@@ -41,36 +41,22 @@ defmodule GothamWeb.WorkingTimeController do
     end
   end
 
-  def post(conn, %{"userID" => userID, "working_time" => working_time_params}) do
-    with {:ok, %WorkingTime{} = working_time} <- WorkingTimeController.create_working_time(working_time_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.working_time_path(conn, :show, working_time))
-      |> render("show.json", working_time: working_time)
-    end
-  end
+  def createUserWorkingTime(conn, %{"id" => id}) do
 
-  def aled(conn, %{"id" => id}, %{"start" => start}, %{"end" => fin}) do
-    start = WorkingTimeController.get_working_time!(start)
-    fin = WorkingTimeController.get_working_time!(fin)
-    id = WorkingTimeController.get_working_time!(id)
-    Logger.info "content #{(start)}, #{(fin)}, #{(id)}"
-    render(conn, "show.json", start: start, end: fin, id: id)
-  end
-
-  def createUserWorkingTime(conn, %{"start" => start}, %{"end" => fin}, %{"id" => id}) do
+    start = conn.body_params["working_time"]["start"]
+    fin = conn.body_params["working_time"]["end"]
     try do
       with {:ok, %WorkingTime{} = working_time} <- WorkingTimeController.create_work(start,fin, id) do
         json(conn, %{
           "data" => %{
             "start" => working_time.start,
             "end" => working_time.end,
-            "user" => working_time.user,
+            "user" => working_time.user
           }
         })
       end
     rescue
-      e in Ecto.ConstraintError ->
+      _e in Ecto.ConstraintError ->
         json(conn, %{"error" => "Validation failed"})
       true -> json(conn, %{"error" => "error"})
     end
